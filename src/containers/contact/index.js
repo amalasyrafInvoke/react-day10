169c07238dd5c0f685db8419db8323ac0c5609a5
amalasyrafs <57763629+amalasyrafs@users.noreply.github.com>
2021-11-24 10:56:21 +0800

    add status to refresh control

--- src/containers/contact/index.js
+++ src/containers/contact/index.js
@@ -6,6 +6,7 @@ import { useDispatch, useSelector } from 'react-redux';
 import { addCount, minusCount } from '../../reducers/countReducer';
 import ContactList from '../../components/contactList';
 import CONTACTDATA from '../../resources/restaurants.json';
+import { Feather } from '@expo/vector-icons';
 
 const wait = (timeout) => {
   return new Promise((resolve) => setTimeout(resolve, timeout));
@@ -27,19 +28,27 @@ export default function Contact() {
 
   return (
     <View style={tw`flex-1`}>
-      <View style={tw`w-full h-32 flex items-center justify-center`}>
+      <View style={tw`w-full flex items-center my-4 justify-center`}>
         <Text>Count: {count}</Text>
-        <View style={tw`flex flex-row items-center justify-center`}>
-          <Pressable onPress={() => dispatch(addCount(2))}>
-            <Text>Add</Text>
+        <View style={tw`flex flex-row items-center justify-center my-2`}>
+          <Pressable style={tw`mx-1`} onPress={() => dispatch(addCount(1))}>
+            <Feather name='plus-square' size={24} color='black' />
           </Pressable>
-          <Pressable onPress={() => dispatch(minusCount(4))}>
-            <Text>Minus</Text>
+          <Pressable style={tw`mx-1`} onPress={() => dispatch(minusCount(1))}>
+            <Feather name='minus-square' size={24} color='black' />
           </Pressable>
         </View>
       </View>
+      <View style={tw`flex items-center justify-center my-1`}>
+        <Text style={tw`font-semibold`}>Status:</Text>
+        <Text>
+          {refreshing
+            ? 'Updating ...'
+            : 'You are good to go!'}
+        </Text>
+      </View>
       <View style={tw`flex items-center justify-center my-4`}>
-        <Text>Last Updated:</Text>
+        <Text style={tw`font-semibold`}>Last Updated:</Text>
         <Text>{lastUpdated}</Text>
       </View>
       <SwipeListView
